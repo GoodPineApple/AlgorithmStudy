9054f0b67435d0cb6201b7780b751d98fc56f526
Taemin Kim <taemin3000@naver.com>
2017-05-06 15:36:50 +0900

    subString

---
+++ src/subString/Solution.java
@@ -0,0 +1,15 @@
+package subString;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        System.out.print(S.substring(start,end)); // String.substring(시작index,끝나는index) 시작index부터 끝index까지의 String을 출력하라
+    }
+}
