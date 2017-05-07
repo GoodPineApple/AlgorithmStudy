e5a62ad563d72a9a0692953f846b212584ef446f
Taemin Kim <taemin3000@naver.com>
2017-05-07 21:11:47 +0900

    String reverse

---
+++ src/string_reverse/Solution.java
@@ -0,0 +1,13 @@
+package string_reverse;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String revA = new StringBuilder(A).reverse().toString();  // String값을 거꾸로 하는 메소드!!!
+        System.out.println(((A.compareTo(revA) == 0)) ? "Yes" : "No");
+    }
+}
