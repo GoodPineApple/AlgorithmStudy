a7b3b4d59cd67e8d40e238db6aa689f2819bed30
Taemin Kim <taemin3000@naver.com>
2017-05-29 21:34:38 +0900

    datastructures 1d array

---
+++ src/datastructures/array_1d/Solution.java
@@ -0,0 +1,25 @@
+package datastructures.array_1d;
+
+import java.util.Scanner;
+
+/**
+ * Created by taemi on 2017-05-29.
+ * https://www.hackerrank.com/challenges/java-1d-array-introduction
+ */
+public class Solution {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] int_arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            int_arr[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        for(int i=0; i<int_arr.length; i++){
+            System.out.println(int_arr[i]);
+        }
+    }
+}
