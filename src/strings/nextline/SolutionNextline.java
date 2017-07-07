1a7377a73eebdc1e1f30c9e890db2ddf3b9a7c79
Taemin Kim <taemin3000@naver.com>
2017-07-07 15:50:01 +0900

    solve nextLine() and next()

--- src/strings/nextline/SolutionNextline.java
+++ src/strings/nextline/SolutionNextline.java
@@ -19,19 +19,19 @@ public class SolutionNextline {
         for (int i = 0; i < n; i++)
         {
             System.out.print("Enter student name: ");
-            b[i] = q.nextLine();
-
-            q.next();
+            b[i] = q.next();
             System.out.print("Enter student number: ");
-
             a[i] = q.nextInt();
         }
 
 
-        for (int i : a)
-            System.out.print(i + " ");
+        for (int i : a) System.out.print(i + " ");
         System.out.println();
-        for (String j : b)
-            System.out.print(j + " ");
+        for (String j : b) System.out.print(j + " ");
+        System.out.println();
+        System.out.println("a[0]의 값: " + a[0]);
+        System.out.println("a[1]의 값: " + a[1]);
+        System.out.println("b[0]의 값: " + b[0]);
+        System.out.println("b[1]의 값: " + b[1]);
     }
 }
