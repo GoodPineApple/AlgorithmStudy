d515ef88725b406cbb7bce71318dff8932fc14a3
Taemin Kim <taemin3000@naver.com>
2017-07-19 11:59:08 +0900

    make new comparator issue

---
+++ src/datastructures/comparator/Solution.java
@@ -0,0 +1,11 @@
+package datastructures.comparator;
+
+/**
+ * Created by taemi on 2017-07-19.
+ * https://www.hackerrank.com/challenges/java-comparator?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
