ce852dd8a3dcdea104cf9d3dd2499fdc7a7d895a
Taemin Kim <taemin3000@naver.com>
2017-07-20 18:49:12 +0900

    solve dquqe problem

--- src/datastructures/deque/Solution.java
+++ src/datastructures/deque/Solution.java
@@ -1,8 +1,6 @@
 package datastructures.deque;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by taemi on 2017-07-20.
@@ -11,13 +9,26 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Deque deque = new ArrayDeque();
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+
         int n = in.nextInt();
         int m = in.nextInt();
+        int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
+            int input = in.nextInt();
 
+            deque.add(input);
+            set.add(input);
+
+            if (deque.size() == m) {
+                if (set.size() > max) max = set.size();
+                int first = deque.remove();
+                if (!deque.contains(first)) set.remove(first);
+            }
         }
+
+        System.out.println(max);
     }
 }
