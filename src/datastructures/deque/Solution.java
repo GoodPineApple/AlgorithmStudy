4f2453f7f0a4bb91b31b7b110619645407a7ab67
Taemin Kim <taemin3000@naver.com>
2017-07-20 18:20:04 +0900

    copy problem about deque

--- src/datastructures/deque/Solution.java
+++ src/datastructures/deque/Solution.java
@@ -1,7 +1,23 @@
 package datastructures.deque;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 /**
  * Created by taemi on 2017-07-20.
+ * https://www.hackerrank.com/challenges/java-dequeue?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
  */
 public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+
+        }
+    }
 }
