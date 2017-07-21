a2b9bf500b40dfb27133a8ef08b9da640de90f84
Taemin Kim <taemin3000@naver.com>
2017-07-21 23:40:31 +0900

    ready for linkedlist problem

---
+++ src/datastructures/linkedlist/printelement/Solution.java
@@ -0,0 +1,10 @@
+package datastructures.linkedlist.printelement;
+
+/**
+ * Created by taemi on 2017-07-21.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+}
