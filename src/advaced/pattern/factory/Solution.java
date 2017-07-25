61c84ca713f3f1d1f0402de0977a7dd7fd17c062
Taemin Kim <taemin3000@naver.com>
2017-07-25 10:23:01 +0900

    create java advanced and pattern.factory package

---
+++ src/advaced/pattern/factory/Solution.java
@@ -0,0 +1,9 @@
+package advaced.pattern.factory;
+
+/**
+ * Created by taemi on 2017-07-25.
+ * https://www.hackerrank.com/challenges/java-factory?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=3-day-campaign
+ */
+public class Solution {
+    
+}
