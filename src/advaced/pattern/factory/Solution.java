1c57446fa17cafb2bcd711a4919a7de0ef344d50
Taemin Kim <taemin3000@naver.com>
2017-07-25 10:54:33 +0900

    solve java factory pattern

--- src/advaced/pattern/factory/Solution.java
+++ src/advaced/pattern/factory/Solution.java
@@ -1,9 +1,78 @@
 package advaced.pattern.factory;
 
+import java.security.Permission;
+import java.util.Scanner;
+
 /**
  * Created by taemi on 2017-07-25.
  * https://www.hackerrank.com/challenges/java-factory?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=3-day-campaign
  */
+interface Food {
+    public String getType();
+}
+
+class Pizza implements Food {
+    public String getType() {
+        return "Someone ordered a Fast Food!";
+    }
+}
+
+class Cake implements Food {
+    public String getType() {
+        return "Someone ordered a Dessert!";
+    }
+}
+
+class FoodFactory {
+    public Food getFood(String order) {
+        switch(order){
+            case("pizza") : return new Pizza();
+            case("cake") : return new Cake();
+            default : return null;
+        }
+    }
+}
+
 public class Solution {
-    
+
+    public static void main(String args[]){
+        Do_Not_Terminate.forbidExit();
+
+        try{
+
+            Scanner sc=new Scanner(System.in);
+            //creating the factory
+            FoodFactory foodFactory = new FoodFactory();
+
+            //factory instantiates an object
+            Food food = foodFactory.getFood(sc.nextLine());
+
+
+            System.out.println("The factory returned "+food.getClass());
+            System.out.println(food.getType());
+        }
+        catch (Do_Not_Terminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+
+}
+class Do_Not_Terminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1L;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
 }
