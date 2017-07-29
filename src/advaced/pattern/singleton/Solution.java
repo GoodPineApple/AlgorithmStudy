7862257a7a65e7194709fb7c0b4dae26da33f983
Taemin Kim <taemin3000@naver.com>
2017-07-29 15:32:53 +0900

    add comment

--- src/advaced/pattern/singleton/Solution.java
+++ src/advaced/pattern/singleton/Solution.java
@@ -2,6 +2,7 @@ package advaced.pattern.singleton;
 
 /**
  * Created by taemi on 2017-07-29.
+ * https://www.hackerrank.com/challenges/java-singleton?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=3-day-campaign
  */
 public class Solution {
 
