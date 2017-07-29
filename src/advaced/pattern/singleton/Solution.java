ed8a7933bc6e271a428b5aa376bc47b7d592408f
Taemin Kim <taemin3000@naver.com>
2017-07-29 15:58:39 +0900

    Cant understand about what means to do

--- src/advaced/pattern/singleton/Solution.java
+++ src/advaced/pattern/singleton/Solution.java
@@ -3,7 +3,11 @@ package advaced.pattern.singleton;
 /**
  * Created by taemi on 2017-07-29.
  * https://www.hackerrank.com/challenges/java-singleton?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=3-day-campaign
+ *
+ private Singleton() {}
+ public String str;
+ private static Singleton instance = new Singleton();
+ public static Singleton getSingleInstance() { return instance; }
  */
 public class Solution {
-
 }
