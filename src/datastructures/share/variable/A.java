6406a4e9cd313d480c8fa8b38a2ad15f8cf448c0
Taemin Kim <taemin3000@naver.com>
2017-08-01 10:27:04 +0900

    check sharing variable by global valiable

---
+++ src/datastructures/share/variable/A.java
@@ -0,0 +1,14 @@
+package datastructures.share.variable;
+
+/**
+ * Created by taemi on 2017-08-01.
+ */
+public class A {
+    int num = 10;
+    public void assignNum(){
+        num = 15;
+    }
+    public void prnNum(){
+        System.out.println(num);
+    }
+}
---
+++ src/datastructures/share/variable/B.java
@@ -0,0 +1,14 @@
+package datastructures.share.variable;
+
+/**
+ * Created by taemi on 2017-08-01.
+ */
+public class B {
+    public static void main(String[] args) {
+        A objectA = new A();
+        System.out.println(objectA.num);
+        objectA.assignNum();
+        objectA.prnNum();
+    }
+
+}
