dc916d2283f1da5412d95dc777a899087d2bb9b0
Taemin Kim <taemin3000@naver.com>
2017-08-10 17:57:45 +0900

    HandleException2

---
+++ src/java_employee_test/ATestClass.java
@@ -0,0 +1,23 @@
+package java_employee_test;
+
+/**
+ * Created by taemi on 2017-08-10.
+ */
+
+//①	public
+//②	private
+//③	abstract
+//④	final
+
+final class ATestClass {
+
+    private class tt {
+        int ttd = 2;
+    }
+
+    public static void main(String[] argv) {
+
+    }
+}
+
+
