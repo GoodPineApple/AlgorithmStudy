7a00c8ee7af57a3685f2749de3a0dd8fa2850ebb
Taemin Kim <taemin3000@naver.com>
2017-08-14 10:30:11 +0900

    comment problem url

--- src/advaced/prime_checker/PrimeChecker.java
+++ src/advaced/prime_checker/PrimeChecker.java
@@ -2,6 +2,7 @@ package advaced.prime_checker;
 
 /**
  * Created by taemi on 2017-08-14.
+ * https://www.hackerrank.com/challenges/prime-checker?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=15-day-campaign
  */
 public class PrimeChecker {
 }
