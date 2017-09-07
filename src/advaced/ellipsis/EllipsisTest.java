e5e0e217c9d086e9a707868cdc2c27a60a67af4e
Taemin Kim <taemin3000@naver.com>
2017-09-07 09:49:28 +0900

    create warmup package and simple array sum

--- src/advaced/ellipsis/EllipsisTest.java
+++ src/advaced/ellipsis/EllipsisTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.CoreMatchers.*;
 public class EllipsisTest {
     @Test
     public void testEllipsis() {
-        assertThat(lengthEllipsis(3, 4, 5, 6), is(4));
+        assertThat(lengthEllipsis(3, 4, 5, 6), is(4));      // assertThat(A, is(B)) A의 값이 B인지 확인한다.
         assertThat(countEllipsis(2, 3, 4, 5, 6), is(20));
         assertThat(stringEllipsis("Korea", "Japan", "China"), is("Korea is Strong country."));
     }
